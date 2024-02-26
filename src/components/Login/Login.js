054d947c75dd0960d6a77c58cd21c6698cdf495e
20120430 <badang0509@gmail.com>
2024-02-26 18:01:42 +0700

    Login interface

---
+++ src/components/Login/Login.js
@@ -0,0 +1,91 @@
+import "./Login.scss";
+export default function Login() {
+  return (
+    <section class="background-radial-gradient overflow-hidden">
+      <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+        <div class="row gx-lg-5 align-items-center mb-5">
+          <div class="col-lg-7 mb-5 mb-lg-0" style={{ zIndex: "10" }}>
+            <h1
+              class="my-5 display-5 fw-bold ls-tight"
+              style={{ color: "hsl(218, 81%, 95%)" }}
+            >
+              Lựa chọn tốt nhất <br />
+              <span style={{ color: "hsl(218, 81%, 75%)" }}>
+                Cho Salon Oto của bạn
+              </span>
+            </h1>
+            <p class="mb-4 opacity-70" style={{ color: "hsl(218, 81%, 85%)" }}>
+              Chúng tôi cung cấp cho bạn các gói dịch vụ tốt nhất giúp quản lí
+              bạn quản lí Salon Oto của mình một cách hiệu quả
+            </p>
+          </div>
+
+          <div class="col-lg-5 mb-5 mb-lg-0 position-relative">
+            <div
+              id="radius-shape-1"
+              class="position-absolute rounded-circle shadow-5-strong"
+            ></div>
+            <div
+              id="radius-shape-2"
+              class="position-absolute shadow-5-strong"
+            ></div>
+
+            <div class="card bg-glass">
+              <div class="card-body px-1 py-5">
+                <form>
+                  <div class="d-flex justify-content-center">
+                    <div class="form-outline mb-4 w-75">
+                      <label class="form-label" for="username">
+                        Tên tài khoản
+                      </label>
+                      <input type="email" id="username" class="form-control" />
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-center">
+                    <div class="form-outline mb-4 w-75">
+                      <label class="form-label" for="password">
+                        Mật khẩu
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
+                  <div class="text-center">
+                    <button
+                      type="submit"
+                      class="btn btn-primary btn-block mb-4 w-75"
+                    >
+                      Đăng nhập
+                    </button>
+                  </div>
+
+                  <div class="text-center">
+                    <p>Hoặc đăng nhập với:</p>
+                    <button
+                      class="btn btn-block btn-primary w-75"
+                      style={{ backgroundColor: "#dd4b39;" }}
+                      type="submit"
+                    >
+                      <i class="fab fa-google me-2"></i> Đăng nhập với google
+                    </button>
+                    <button
+                      class="btn btn-block btn-danger mb-2 w-75 mt-2"
+                      style={{ backgroundColor: "#3b5998;" }}
+                      type="submit"
+                    >
+                      <i class="fab fa-facebook-f me-2"></i>Đăng nhập với
+                      facebook
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
