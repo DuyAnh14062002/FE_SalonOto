151efcd57377a7ebcb15dc19c699c1d012006532
duyanh <87406392+DuyAnh14062002@users.noreply.github.com>
2024-02-26 18:30:16 +0700

    Register interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Login from "./components/Login";
-
+import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register/>
     </div>
   );
 }
